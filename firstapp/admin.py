1d7b5d44725f8afda52a830f005a3c3a147d853d
Rajanpal01 <rajan975390@gmail.com>
2022-10-25 12:54:03 +0530

    git initiallise

---
+++ firstapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Project_details,Projects_ideas,Form_data
+
+
+# Register your models here.
+admin.site.register(Project_details)
+admin.site.register(Projects_ideas)
+admin.site.register(Form_data)
